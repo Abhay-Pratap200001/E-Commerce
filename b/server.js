74b70014d401047d5cc3c8f9fd6ee0fddcbc86eb
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-10-20 18:06:03 +0530

    DB connected successfully to mongodb

--- b/server.js
+++ b/server.js
@@ -1,1 +1,29 @@
-//continu...
+import express from 'express'
+import dotenv from 'dotenv'
+import authRouter from './routes/auth.Route.js'
+import { connectDB } from './lib/dbConncrtion.js';
+
+dotenv.config();
+
+const app = express()
+const PORT = process.env.PORT || 2000
+
+app.use('/api/auth', authRouter)
+
+
+connectDB().then(() => {
+    const server = app.listen(PORT, () => {
+      console.log(`✅ Server running at http://localhost:${PORT}`);
+    });
+
+    // server error handling
+    server.on("error", (error) => {
+      console.error("❌ Server Error:", error);
+      process.exit(1); // Exit the app if server fails
+    });
+  })
+  .catch((error) => {
+    console.error("❌ MongoDB Connection Failed:", error);
+    process.exit(1); // Exit app if DB connection fails
+  });
+
