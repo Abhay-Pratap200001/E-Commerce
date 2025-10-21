d4bac9beff888c319199093b7e86c4937f5030bf
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-10-21 15:53:38 +0530

    ....

---
+++ b/utils/api.Error.js
@@ -0,0 +1,19 @@
+//For sending error with node.js error class 
+class ApiError extends Error {
+  constructor(statusCode, message = "Something went wrong", errors = [], stack = ''){
+    super(message)
+    this.statusCode = statusCode,
+    this.data = null,
+    this.message = message,
+    this.succss = false;
+    this.errors = errors
+
+    if (stack) {
+       this.stack = stack 
+    }else{
+        Error.captureStackTrace(this, this.constructor)
+    }
+  }
+}
+
+export {ApiError}
