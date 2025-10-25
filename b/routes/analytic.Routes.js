417c6ee465db96f02f9a552ef1d1f5ee94eec4fb
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-10-25 21:18:56 +0530

    create payment and analytic route

---
+++ b/routes/analytic.Routes.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import { adminRoute, protectRoute } from '../middleware/auth.middleware.js'
+import { getAnalyticsData, getDailySalesData } from '../controllers/analytic.controller.js'
+import { ApiError } from '../utils/api.Error.js'
+
+const router = express.Router()
+
+//  Route: GET /analytics (protected + admin only)
+router.get('/', protectRoute, adminRoute, async (req, res) => {
+  try {
+    //  Get overall analytics (total users, products, sales, revenue)
+    const analyticData = await getAnalyticsData();
+
+    //  Get last 7 days' date range
+    const endDate = new Date();
+    const startDate = new Date(endDate.getTime() - 7 * 24 * 60 * 60 * 1000);
+
+    // Fetch sales & revenue for each day in this range
+    const dailySalesData = await getDailySalesData(startDate, endDate);
+
+    // Send both sets of data together
+    res.json({
+      analyticData,
+      dailySalesData
+    });
+  } catch (error) {
+    throw new ApiError(500, "server error");
+  }
+});
+
+export default router;
---
+++ b/routes/payment.Route.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { protectRoute } from '../middleware/auth.middleware.js'
+import { checkoutSuccess, createCheckoutSession } from '../controllers/payment.controller.js'
+
+
+const router = express.Router()
+
+
+//user will send req to this end point to buy something
+router.post('/create-checkout-session',protectRoute, createCheckoutSession )
+router.post('/checkout-success',protectRoute, checkoutSuccess)
+
+
+
+export default router 
