a5b2901baa0ae88f1e1b97e8144d2d5d3e316977
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-11-10 19:56:01 +0530

    ...

--- b/server.js
+++ b/server.js
@@ -19,7 +19,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const _dirname = path.resolve()
+const __dirname = path.resolve()
 
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
@@ -33,7 +33,7 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
 
-if (process.env.NODE_ENV === "PRODUCTION") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/f/dist")))
 
   app.get("*", (req, res) => {
