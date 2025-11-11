23eac4c9722ab1b7c10eb343b8aa81375f2f40ce
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-11-11 09:19:59 +0530

    addes product category images

--- f/src/Pages/HomePage.jsx
+++ f/src/Pages/HomePage.jsx
@@ -4,10 +4,17 @@ import ProductTypes from '../components/ProductTypes';
 import jeans from "../assets/jeans.jpg";
 import tshirts from "../assets/tshirts.jpg";
 import shoes from "../assets/shoes.jpg";
-import glasses from "../assets/glasses.png"; // fixed here
+import glasses from "../assets/glasses.png"; 
 import jackets from "../assets/jackets.jpg";
 import suits from "../assets/suits.jpg";
 import bags from "../assets/bags.jpg";
+import watches from "../assets/watches.webp"
+import hoodies from "../assets/hoodies.webp"
+import accessories from "../assets/accessories.jpg"
+import perfumes from "../assets/perfumes.webp"
+import haircare from "../assets/haircare.webp"
+import skincare from "../assets/skincare.jpg"
+import electronics from "../assets/electronics.avif"
 import { useProductStore } from '../stores/useProductStore';
 import FeaturedProducts from '../components/FeaturedProducts';
 
@@ -20,10 +27,14 @@ import FeaturedProducts from '../components/FeaturedProducts';
   { href: "/jackets", name: "Jackets", imageUrl: jackets },
   { href: "/suits", name: "Suits", imageUrl: suits },
   { href: "/bags", name: "Bags", imageUrl: bags },
-  // { href: "/watches", name: "Watches", imageUrl: watches },
-  // { href: "/hoodies", name: "Hoodies", imageUrl: hoodies },
-  // { href: "/accessories", name: "Accessories", imageUrl: accessories },
-  // { href: "/perfumes", name: "Perfumes", imageUrl: perfumes },
+  { href: "/watches", name: "Watches", imageUrl: watches },
+  { href: "/hoodies", name: "Hoodies", imageUrl: hoodies },
+  { href: "/accessories", name: "Accessories", imageUrl: accessories },
+  { href: "/perfumes", name: "Perfumes", imageUrl: perfumes },
+  { href: "/haircare", name: "haircare", imageUrl: haircare },
+  { href: "/skincare", name: "skincare", imageUrl: skincare },
+  { href: "/electronics", name: "electronics", imageUrl: electronics },
+
 ];
 
 
@@ -38,7 +49,7 @@ const HomePages = () => {
   },[fetchFeaturedProducts])
 
   return (
-    <div className='relative min-h-screen text-white overflow-hidden'>
+    <div className='relative min-h-screen overflow-hidden'>
       <div className='relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16'>
         <h1 className='text-center text-5xl sm:text-6xl font-bold text-emerald-400 mb-4'>
           Explore Our Category
@@ -53,7 +64,7 @@ const HomePages = () => {
           ))}
         </div>
 
-{!loading && products.length > 0 && <FeaturedProducts featuredProducts={products} />}
+          {!loading && products.length > 0 && <FeaturedProducts featuredProducts={products} />}
 
       </div>
     </div>
